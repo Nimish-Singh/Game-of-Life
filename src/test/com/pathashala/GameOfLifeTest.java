c199baf9663fbf20ce2acac83530601e73eb93d9
Nimish Singh <nimishs@thoughtworks.com>
2017-12-24 13:45:08 +0530

    Nimish: Added tests for all cases of GOL

--- src/test/com/pathashala/GameOfLifeTest.java
+++ src/test/com/pathashala/GameOfLifeTest.java
@@ -44,6 +44,42 @@ class GameOfLifeTest {
   }
 
   @Test
+  void expectBlockPatternToGiveStillLifeOutput() {
+    GameOfLife game = new GameOfLife();
+    ArrayList<Cell> cellList = new ArrayList<>();
+    cellList.add(new Cell(1, 1));
+    cellList.add(new Cell(1, 2));
+    cellList.add(new Cell(2, 1));
+    cellList.add(new Cell(2, 2));
+    Grid initial = game.createSeed(cellList);
+    HashMap<Cell, CellStatus> latter = new HashMap<>();
+    latter.put(new Cell(1, 1), new CellStatus(true));
+    latter.put(new Cell(1, 2), new CellStatus(true));
+    latter.put(new Cell(2, 1), new CellStatus(true));
+    latter.put(new Cell(2, 2), new CellStatus(true));
+    assertEquals(new Grid(latter), game.tick(initial));
+  }
+
+  @Test
+  void expectBoatPatternToGiveStillLifeOutput() {
+    GameOfLife game = new GameOfLife();
+    ArrayList<Cell> cellList = new ArrayList<>();
+    cellList.add(new Cell(0, 1));
+    cellList.add(new Cell(1, 0));
+    cellList.add(new Cell(2, 1));
+    cellList.add(new Cell(0, 2));
+    cellList.add(new Cell(1, 2));
+    Grid initial = game.createSeed(cellList);
+    HashMap<Cell, CellStatus> latter = new HashMap<>();
+    latter.put(new Cell(0, 1), new CellStatus(true));
+    latter.put(new Cell(1, 0), new CellStatus(true));
+    latter.put(new Cell(2, 1), new CellStatus(true));
+    latter.put(new Cell(0, 2), new CellStatus(true));
+    latter.put(new Cell(1, 2), new CellStatus(true));
+    assertEquals(new Grid(latter), game.tick(initial));
+  }
+
+  @Test
   void expectBlinkerPatternToGiveOscillatorOutput() {
     GameOfLife game = new GameOfLife();
     ArrayList<Cell> cellList = new ArrayList<>();
@@ -55,6 +91,27 @@ class GameOfLifeTest {
     latter.put(new Cell(1, 1), new CellStatus(true));
     latter.put(new Cell(0, 1), new CellStatus(true));
     latter.put(new Cell(2, 1), new CellStatus(true));
+    assertEquals(new Grid(latter), game.tick(initial));
+  }
+
+  @Test
+  void expectToadPatternToGiveTwoPhaseOscillatorOutput() {
+    GameOfLife game = new GameOfLife();
+    ArrayList<Cell> cellList = new ArrayList<>();
+    cellList.add(new Cell(1, 1));
+    cellList.add(new Cell(1, 2));
+    cellList.add(new Cell(1, 3));
+    cellList.add(new Cell(2, 2));
+    cellList.add(new Cell(2, 3));
+    cellList.add(new Cell(2, 4));
+    Grid initial = game.createSeed(cellList);
+    HashMap<Cell, CellStatus> latter = new HashMap<>();
+    latter.put(new Cell(0, 2), new CellStatus(true));
+    latter.put(new Cell(1, 1), new CellStatus(true));
+    latter.put(new Cell(1, 4), new CellStatus(true));
+    latter.put(new Cell(2, 1), new CellStatus(true));
+    latter.put(new Cell(2, 4), new CellStatus(true));
+    latter.put(new Cell(3, 3), new CellStatus(true));
     assertEquals(new Grid(latter), game.tick(initial));
   }
 }
